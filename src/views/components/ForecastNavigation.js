12b9ad8dfd97f65b27df852ada341519500c7fa6
sergiuplesco <plescosergiu@gmail.com>
2021-05-05 12:25:17 +0300

    kkk

--- src/views/components/ForecastNavigation.js
+++ src/views/components/ForecastNavigation.js
@@ -3,11 +3,7 @@ import { NavLink } from "react-router-dom";
 const ForecastNavigation = () => {
 	return (
 		<div className="forecast">
-			<NavLink
-				className="forecast-link"
-				activeClassName="active"
-				to={process.env.PUBLIC_URL + "/today"}
-			>
+			<NavLink className="forecast-link" activeClassName="active" to="/today">
 				<p>Today</p>
 			</NavLink>
 			<NavLink className="forecast-link" activeClassName="active" to="/tomorrow">
